3b7335f0e5477de7406100c2d29f8e7198d5dd56
xoraus <imsafay@gmail.com>
2024-01-24 23:56:12 +0530

    Adds Model Mapper

--- src/main/java/io/xoraus/CyberScribeHub/CyberScribeHubApplication.java
+++ src/main/java/io/xoraus/CyberScribeHub/CyberScribeHubApplication.java
@@ -1,13 +1,20 @@
 package io.xoraus.CyberScribeHub;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CyberScribeHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CyberScribeHubApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
