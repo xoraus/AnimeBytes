b709979e04b6380b53d9cbc0e525e0c4197cdf4f
xoraus <imsafay@gmail.com>
2024-01-29 15:52:05 +0530

    Cleans dir

--- src/main/java/io/xoraus/CyberScribeHub/services/CommentService.java
+++
@@ -1,8 +0,0 @@
-package io.xoraus.CyberScribeHub.services;
-
-import io.xoraus.CyberScribeHub.payloads.CommentDto;
-
-public interface CommentService {
-    CommentDto createComment(CommentDto commentDto, Integer postId);
-    void deleteComment(Integer commentId);
-}
